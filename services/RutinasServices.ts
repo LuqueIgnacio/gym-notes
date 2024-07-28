c95c983432a86ee610d8085384d3fb9f4f4efd83
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-07-28 19:40:36 -0300

    Added types for RutinaServices

--- services/RutinasServices.ts
+++ services/RutinasServices.ts
@@ -2,12 +2,13 @@ import db from "../db/database"
 import { Users, Rutina, Ejercicio, RutinaToEjercicio } from "@/db/schema"
 import { eq } from "drizzle-orm"
 import uuid from "react-native-uuid"
+import { EjercicioSeleccionado, Ejercicio as EjercicioType, RutinaType, RutinaToEjercicioType, RutinaWithEjerciciosType, RutinaWithRutinaToEjercicioType, EjercicioDropdownType } from "@/types/types"
 
 export async function getAllRutinas(){
     return await db.select().from(Rutina)
 }
 
-export async function getRutinaWithEjercicios(id){
+export async function getRutinaWithEjercicios(id: any){
     const rutinaEjercicios = await db.query.Rutina.findFirst({
         where: eq(Rutina.id, id),
         with: {
@@ -18,49 +19,51 @@ export async function getRutinaWithEjercicios(id){
             }
         }
     })
+    if(!rutinaEjercicios){
+        return
+    }
     const ejercicios = rutinaEjercicios?.rutinaToEjercicio.map(e => e.ejercicio)
-    return {id: rutinaEjercicios?.id, name: rutinaEjercicios?.name, ejercicios: ejercicios}
+    const rutinaWithEjercicios: RutinaWithEjerciciosType = {id: rutinaEjercicios?.id, name: rutinaEjercicios?.name, ejercicios: ejercicios}
+    return rutinaWithEjercicios
 }
 
-export async function addRutina(rutina){
+export async function addRutina(rutina: RutinaWithRutinaToEjercicioType){
     await db.insert(Rutina).values({id: rutina.id, name: rutina.name})
     await db.insert(RutinaToEjercicio).values(rutina.ejercicios)
 }
 
-export async function editRutina(rutina){
+export async function editRutina(rutina: RutinaWithRutinaToEjercicioType){
     await db.delete(RutinaToEjercicio).where(eq(RutinaToEjercicio.rutinaId, rutina.id))
     await db.insert(RutinaToEjercicio).values(rutina.ejercicios)
 }
 
-export async function deleteRutina(rutina){
-    return await db.delete(Rutina).where(eq(Rutina.id, rutina.id)) 
+export async function deleteRutina(idRutina: unknown){
+    return await db.delete(Rutina).where(eq(Rutina.id, idRutina)) 
 }
 
 export async function getAllEjercicios(){
-    return await db.select().from(Ejercicio)
-}
-
-export async function getAllEjerciciosForDropwdown() {
-    let ejercicios = await db.select().from(Ejercicio)
-    ejercicios = ejercicios.map( e => {
-        e.title = e.name
-        e.isSelected = false
-        return e
-    })
+    const ejercicios : EjercicioType[] = await db.select().from(Ejercicio)
     return ejercicios
 }
 
-export function prepareDataForModifyRutinaComponent(rutinaWithEjercicios, ejerciciosDropdown){
-    const newEjerciciosSeleccionados = rutinaWithEjercicios.ejercicios?.map(e => {
-        e.key = e.id
-        return e
+export async function getAllEjerciciosForDropwdown() {
+    let ejercicios = await getAllEjercicios()
+    const ejerciciosDropdown = ejercicios.map( e => {
+        const ejercicioDropdown : EjercicioDropdownType = {
+            ...e,
+            title: e.name,
+            isSelected: false
+        }
+        return ejercicioDropdown
     })
-    ejerciciosDropdown = ejerciciosDropdown.map(e => {
-        e.isSelected = false
-        e.title = e.name
-        return e
-      })
-      newEjerciciosSeleccionados.forEach(es =>{
+    return ejerciciosDropdown
+}
+
+export function prepareDataForModifyRutinaComponent(rutinaWithEjercicios: RutinaWithEjerciciosType, ejerciciosDropdown: EjercicioDropdownType[]){
+    const newEjerciciosSeleccionados: EjercicioSeleccionado[] = rutinaWithEjercicios.ejercicios.map(e => {
+        return {...e, key: e.id}
+    })
+    newEjerciciosSeleccionados.forEach(es =>{
         const index = ejerciciosDropdown.findIndex(e => e.id === es.id)
         ejerciciosDropdown[index].isSelected = true
     })
@@ -68,8 +71,8 @@ export function prepareDataForModifyRutinaComponent(rutinaWithEjercicios, ejerci
     return {ejerciciosSeleccionados: newEjerciciosSeleccionados, ejerciciosDropdown: ejerciciosDropdown}
 }
 
-export function prepareDataForSaveRutina(rutina, ejercicios){
-    let newEjercicios
+export function prepareDataForSaveRutina(rutina: RutinaType, ejercicios: EjercicioType[]): RutinaWithRutinaToEjercicioType{
+    let newEjercicios: RutinaToEjercicioType[]
     if(rutina.id){
         newEjercicios = ejercicios.slice().map( e => {
             return {ejercicioId: e.id, rutinaId: rutina.id}
