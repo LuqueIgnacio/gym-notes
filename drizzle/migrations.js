a60891549bb35fbfe1fda51ec09eb40c96afb0d7
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-08-07 19:32:31 -0300

    Fix an error in the database

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,13 +1,12 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_shocking_sister_grimm.sql';
-
+import m0000 from './0000_certain_loki.sql';
 
   export default {
     journal,
     migrations: {
-      m0000,
+      m0000
     }
   }
   
